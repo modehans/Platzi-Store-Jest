24447fe13013b57caa5d9b94eb9d23bbf43a32ca
modehans <monicaglezsanch@gmail.com>
2022-11-02 12:20:13 +0100

    Create test to remove front cart in action test

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -2,12 +2,21 @@ import actions from '../actions/';
 import ProductMock from './mocks/ProductMock';
 
 describe('Actions', () => {
+  const payload = ProductMock;
+
   test('addToCart Action', () => {
-    const payload = ProductMock;
     const expected = {
       type: 'ADD_TO_CART',
       payload,
     };
     expect(actions.addToCart(payload)).toEqual(expected);
+  });
+
+  test('removeFromCart', () => {
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
   });
 });
