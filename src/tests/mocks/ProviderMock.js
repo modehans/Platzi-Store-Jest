b0ff48b4dea4023c75c3c9b901f5ecfa258a5023
modehans <monicaglezsanch@gmail.com>
2022-11-02 11:23:18 +0100

    Add ProviderMock and ProductMock

--- src/tests/mocks/ProviderMock.js
+++ src/tests/mocks/ProviderMock.js
@@ -1,10 +1,11 @@
+import 'jsdom-global/register';
 import React from 'react';
 import { createStore } from 'redux';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import initialState from '..../initialState';
-import reducer from '..../reducers';
+import initialState from '../../initialState';
+import reducer from '../../reducers';
 
 const store = createStore(reducer, initialState);
 const history = createBrowserHistory();
