c63c580e913b328b45d8620b190ee524af6071db
modehans <monicaglezsanch@gmail.com>
2022-11-02 12:32:40 +0100

    Add in reducers.test.js add to cart test

--- src/tests/reducers.test.js
+++ src/tests/reducers.test.js
@@ -5,4 +5,19 @@ describe('Reducers', () => {
   test('Retomar initial State', () => {
     expect(reducer({}, '')).toEqual({});
   });
+
+  test('ADD_To_CART', () => {
+    const initialState = {
+      cart: [],
+    };
+    const payload = ProductMock;
+    const action = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    const expected = {
+      cart: [ProductMock],
+    };
+    expect(reducer(initialState, action)).toEqual(expected);
+  });
 });
